d55e026c8cedabdf960d312690d43a12274e9505
juancolchete <juanudkp@gmail.com>
2024-12-09 18:24:46 -0300

    docs: sync changes

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
   outputs = { self, nixpkgs }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix ];
+      modules = [ ./configuration.nix ./hardware-configuration.nix ];
     };
   };
 }
