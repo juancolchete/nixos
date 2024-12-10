a3929244b3a191398ed1f9305eb5e76b4f2d482c
juancolchete <juanudkp@gmail.com>
2024-12-10 18:46:32 -0300

    fix: only get from storage when file not exists

--- apply.sh
+++ apply.sh
@@ -8,11 +8,16 @@ fi
 if [[ -z "share" ]]; then
   read -p 'share: ' share
 fi
-gio copy smb://$server/$share/apply.sh /etc/nixos/apply.sh
-gio copy smb://$server/$share/keys/github ~/.ssh/github
-gio copy smb://$server/$share/keys/github.pub ~/.ssh/github.pub
-gio copy smb://$server/$share/keys/envs/env.nix /etc/nixos/env.nix
-chmod  400 ~/.ssh/github
+if [ -e "~/.ssh/github" ]; then
+  gio copy smb://$server/$share/keys/github ~/.ssh/github
+fi
+if [ -e "~/.ssh/github.pub" ]; then
+  gio copy smb://$server/$share/keys/github.pub ~/.ssh/github.pub
+  chmod  400 ~/.ssh/github
+fi
+if [ -e "/etc/nixos/env.nix" ]; then
+  gio copy smb://$server/$share/keys/envs/env.nix /etc/nixos/env.nix
+fi
 curl https://raw.githubusercontent.com/juancolchete/nixos/refs/heads/main/configuration.nix -o /etc/nixos/configuration.nix 
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
 sudo nix-channel --update 
