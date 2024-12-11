5c18cfa9e2798df2717f8c5ceb63af64e8575dde
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2024-12-10 23:30:37 -0300

    fix: build withouy source .bashrc

--- apply.sh
+++ apply.sh
@@ -2,20 +2,20 @@ if [ -e "/etc/nixos/.env" ]; then
     source /etc/nixos/.env
 fi
 sudo chown -R juanc /etc/nixos
-if [[ ! -z "server" ]]; then
+if [[ -z "$server" ]]; then
   read -p 'server: ' server
 fi
-if [[ ! -z "share" ]]; then
+if [[ -z "$share" ]]; then
   read -p 'share: ' share
 fi
-if [ ! -e "~/.ssh/github" ]; then
+if [ ! -f ~/.ssh/github ]; then
   gio copy smb://$server/$share/keys/github ~/.ssh/github
-fi
-if [ ! -e "~/.ssh/github.pub" ]; then
-  gio copy smb://$server/$share/keys/github.pub ~/.ssh/github.pub
   chmod  400 ~/.ssh/github
 fi
-if [ ! -e "/etc/nixos/env.nix" ]; then
+if [ ! -f ~/.ssh/github.pub ]; then
+  gio copy smb://$server/$share/keys/github.pub ~/.ssh/github.pub
+fi
+if [ ! -f /etc/nixos/env.nix ]; then
   gio copy smb://$server/$share/keys/envs/env.nix /etc/nixos/env.nix
 fi
 curl https://raw.githubusercontent.com/juancolchete/nixos/refs/heads/main/configuration.nix -o /etc/nixos/configuration.nix 
@@ -26,9 +26,10 @@ cd /etc/nixos
 git init
 git remote -v | grep -w origin && git remote set-url origin git@github.com:juancolchete/nixos.git || git remote add origin git@github.com:juancolchete/nixos.git
 git branch -m main
-git pull --set-upstream origin main
-rustup use 1.79.0
-[ ! -d "/home/juanc/programs/solana" ] && runuser -u juanc git clone https://github.com/solana-labs/solana /home/juanc/programs/solana
+git pull origin main
+source ~/.bashrc
+mkdir -p /home/juanc/programs
+[ ! -d "/home/juanc/programs/solana" ] && git clone https://github.com/solana-labs/solana /home/juanc/programs/solana
 touch /home/juanc/programs/solana/rust-toolchain.toml
 rm /home/juanc/programs/solana/rust-toolchain.toml
 touch /home/juanc/programs/solana/rust-toolchain.toml
