214343523197e35f53a4cb81258b4d8825eef3e3
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2024-12-13 22:08:07 -0300

    docs: disable suspend

--- configuration.nix
+++ configuration.nix
@@ -194,6 +194,12 @@ in {
   fonts.packages = with pkgs; [
     nerdfonts
   ];
+  systemd.sleep.extraConfig = ''
+   AllowSuspend=no
+   AllowHibernation=no
+   AllowHybridSleep=no
+   AllowSuspendThenHibernate=no
+  '';
   system.stateVersion = "24.11"; # Did you read the comment?
   system.activationScripts.rustup = ''
     PATH=${pkgs.rustup}/bin:/home/juanc/.cargo/bin:${pkgs.curl}/bin:${pkgs.bash}/bin:run/current-system/sw/bin:/nix/store/8rq01kg6lng5d1fz5wd0mfn2yjaww9sq-system-path/bin/tar:/run/current-system/sw/bin/clang:$PATH
