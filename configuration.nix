4467191cbaf3ea247fc048afb79b2dae10f27235
juancolchete <juanudkp@gmail.com>
2025-02-21 19:14:06 -0300

    chore: remove use of x11

--- configuration.nix
+++ configuration.nix
@@ -52,14 +52,6 @@ in {
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.gdm.wayland = false;
-  programs.ssh.forwardX11 = true;
-  programs.ssh.setXAuthLocation = true;
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -109,7 +101,6 @@ in {
       brave
       gitkraken
       tor-browser
-      kitty
       tmux
    ];
   };
